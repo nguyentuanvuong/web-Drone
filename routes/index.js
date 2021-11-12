ef2b6632335926594e4dbde4b0578ce8b272935e
duyvo689 <vothanhduy689@gmail.com>
2021-11-12 10:07:11 +0700

    active class item navbar

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,32 @@
 const express = require('express');
 var router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.render('index');
-  });
-router.get('/connect-camera',(req,res)=>{
+router.get('/', (req, res) => {
+  res.render('index');
+});
+router.get('/connect-camera', (req, res) => {
   res.render('camera');
-    });
+});
 
-router.get('/training',(req,res)=>{
-  res.render('training');
+router.get('/Training', (req, res) => {
+  res.render('Training');
 });
-router.get('/ALlCam',(req,res)=>{
-    res.render('ALlCam');
-  });
-router.get('/Mission',(req,res)=>{
-    res.render('Mission');
+router.get('/ALlCam', (req, res) => {
+  res.render('ALlCam');
 });
-router.get('/lora-map',(req, res)=>{
+router.get('/Mission', (req, res) => {
+  res.render('Mission');
+});
+router.get('/lora-map', (req, res) => {
   res.render('mapLora');
 });
-router.get('/bank',(req,res)=>{
-    res.render('bank');
-  });
-router.get('/chat',(req,res)=>{
-    res.render('bank');
-  });
-router.get('/login',(req,res)=>{
-    res.render('login');
-  });
+router.get('/bank', (req, res) => {
+  res.render('bank');
+});
+router.get('/chat', (req, res) => {
+  res.render('bank');
+});
+router.get('/login', (req, res) => {
+  res.render('login');
+});
 module.exports = router;
