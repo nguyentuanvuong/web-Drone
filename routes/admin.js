d8227ba7fe3f115312a4c55803abb661a6e82d9c
nguyentuanvuong <55832259+nguyentuanvuong@users.noreply.github.com>
2021-11-13 11:46:54 +0700

    Update .gitignore

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,8 @@
 const express = require('express');
 var router = express.Router();
 
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     res.render('admin/index');
-  });
+});
 
 module.exports = router;
--- routes/api.js
+++ routes/api.js
@@ -2,20 +2,46 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const multer = require('multer');
+const fs = require('fs');
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-      cb(null, 'uploads/')
-  },
-  filename: (req, file, cb) => {
-      const { originalname } = file;
-      cb(null, originalname);
-  }
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb) => {
+        if (file.mimetype.split("/")[1] == 'x-zip-compressed') {
+            cb(null, `${makeid(50)}.zip`);
+        } else {
+            cb(new Error("Not a zip File!!"), false);
+        }
+    }
 })
-const upload = multer({ dest: 'uploads/' }); // or simply { dest: 'uploads/' }
 
-router.post('/update/dataset', upload.single('dataset'),(req,res)=>{
-  return res.json({ status: 'OK', uploaded: req.files.length });
+const upload = multer({
+    storage
 });
+
+const dir = './';
+
+router.post('/update-dataset', upload.single('dataset'), (req, res) => {
+    res.json(req.file);
+});
+
+router.get('/list-file',(req, res)=>{
+    const files = fs.readdirSync(dir);
+    res.json(files);
+    
+});
+
+function makeid(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
 
 module.exports = router;
