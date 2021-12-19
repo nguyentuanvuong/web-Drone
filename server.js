41b1a788a7eac099c5045fe8ad85aef5cef13a23
nguyentuanvuong <55832259+nguyentuanvuong@users.noreply.github.com>
2021-12-19 12:13:19 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,17 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const ngrok = require('ngrok');
-const cors = require("cors");
-const mqtt = require('mqtt');
+var express = require('express');
+var app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-const mqttClient = mqtt.connect('mqtt://broker.hivemq.com');
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-// const mqttClient = mqtt.connect('63156a3d1fef45f8826b24ff258bb6e4.s1.eu.hivemq.cloud');
-
-require('dotenv').config();
-
-var httpPort = process.env.HTTP_PORT || 8080;
-
-const httpServer = http.createServer(app);
-
-// const { Server } = require("socket.io");
-// const io = new Server(httpServer);
-
-const io = require("socket.io")(httpServer, {
-    cors: {
-      methods: ["GET", "POST"]
-    }
-  });
-
-var listSocket = [];
-var trainStartus = false;
-
-app.set("view engine", "ejs");
-app.set("views", "./views");
+// make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
-app.use(cors());  
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
+// set the home page route
 const indexRouter = require('./routes/index');
 const adminRouter = require('./routes/admin');
 const apiRouter = require('./routes/api');
@@ -46,78 +23,6 @@ app.use('/api', apiRouter);
 app.use("/", require("./routes/mainRoutes"));
 app.use("/", require("./routes/fileRoutes"));
 
-mqttClient.on('connect',()=>{
-    mqttClient.subscribe('ntvuong');
-    console.log('mqtt Connect');
-    mqttClient.on('message',(topic, msg)=>{
-        io.emit('sensor',msg.toString());
-    });
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
 });
-
-
-io.on('connection', (socket) => {
-    count = io.engine.clientsCount;
-    listSocket.push(socket.id);
-    // console.log('Socket number online', count);
-    io.emit(socket.id, listSocket);
-    socket.broadcast.emit('connect Socket', socket.id);
-
-    socket.on('StreamID', msg => {
-        socket.broadcast.emit('AllCam', msg);
-    });
-
-    socket.on('results',(msg)=>{
-        socket.broadcast.emit('results',msg);
-    });
-
-    socket.on('sensor',(msg)=>{
-        socket.broadcast.emit('sensor',msg);
-    });
-
-    socket.on('trainStartus',(msg)=>{
-        socket.broadcast.emit('dataset','');
-        io.emit('trainStartus',trainStartus);
-    });
-    socket.on('dataset',(msg)=>{
-        socket.broadcast.emit('dataset',msg);
-    });
-    socket.on('training',(msg)=>{
-        if(!trainStartus) {
-            socket.broadcast.emit('training',msg);
-        }
-    });
-    socket.on('TrainingDone',(msg)=>{
-        trainStartus = false;
-        socket.broadcast.emit('trainStartus',trainStartus);
-    });
-    socket.on('TrainingStart',(msg)=>{
-        trainStartus = true;
-        socket.broadcast.emit('trainStartus',trainStartus);
-    });
-    socket.on('CreateTrainingFile',(msg)=>{
-        socket.broadcast.emit('CreateTrainingFile',msg);
-    });
-
-    console.log('connect', socket.id);
-    socket.on('disconnect', () => {
-        console.log('disconnect', socket.id);
-        count = io.engine.clientsCount;
-        // console.log('Socket number online', count);
-        listSocket = listSocket.filter((element) => {
-            return element !== socket.id;
-        });
-        io.emit('disconnect Socket', socket.id);
-    });
-});
-
-httpServer.listen(httpPort, () => {
-    console.log('http listening on *:',httpPort);
-});
-
-// (async function() {
-//     const url = await ngrok.connect({
-//         addr: httpPort,
-//         authtoken: process.env.NGROK_TOKEN
-//     });
-//     console.log(url);
-//   })();
