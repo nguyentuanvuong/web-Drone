472886b910cd3c2108aaa0f34986072ce425e73d
nguyentuanvuong <55832259+nguyentuanvuong@users.noreply.github.com>
2021-12-19 11:56:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ var trainStartus = false;
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(cors());  
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
