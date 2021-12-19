1e26c66b442b99e0017f1080ab4a75104ea8ebef
nguyentuanvuong <55832259+nguyentuanvuong@users.noreply.github.com>
2021-12-19 20:26:34 +0700

    main

--- public/js/training.js
+++ public/js/training.js
@@ -88,11 +88,81 @@ $('#train_model').submit(function (event) {
         btnTrainingCsv.innerHTML = `
         Training
         `;
-
-        textConsole(JSON.stringify(response));
+        Chart(response);
+        // textConsole(JSON.stringify(response));
     });
     event.preventDefault();
 });
+
+function Chart(response){
+
+
+    const data = response.data.trainingInfo;
+    
+    console.log(JSON.stringify(data));
+
+    Highcharts.chart('container', {
+
+        title: {
+            text: 'Solar Employment Growth by Sector, 2010-2016'
+        },
+    
+        subtitle: {
+            text: 'Source: thesolarfoundation.com'
+        },
+    
+        yAxis: {
+            title: {
+                text: 'Number of Employees'
+            }
+        },
+    
+        xAxis: {
+            accessibility: {
+                rangeDescription: 'Range: 2010 to 2017'
+            }
+        },
+    
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+        },
+    
+        plotOptions: {
+            series: {
+                label: {
+                    connectorAllowed: false
+                },
+                pointStart: 2010
+            }
+        },
+    
+        series: [{
+            name: 'loss',
+            data: data.loss
+        }, {
+            name: 'acc',
+            data: data.acc
+        }],
+    
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
+                    }
+                }
+            }]
+        }
+    
+    });
+}
 
 // $('#train_model').submit(function (event) {
 //     btnTrainingCsv.disabled = true;
